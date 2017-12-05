6cea52c173a18798e7d71b6e21853c212b4abb59
run <guangxu2016@foxmail.com>
2017-12-05 22:53:46 +0800

    'twoday'

---
+++ project/src/components/Shopcart/Shopcart.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        这是购物页
+    </div>
+
+</template>
+<script>
+    export default {
+        data() {
+            return {
+
+            }
+        }
+    }
+</script>
+<style scoped>
+
+</style>
