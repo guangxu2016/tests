412c7e83dfdd64caf12efc27490662785f66145d
run <guangxu2016@foxmail.com>
2017-12-06 22:07:29 +0800

    商品列表完成

---
+++ project(shopend)/src/components/App.vue
@@ -0,0 +1,50 @@
+<template>
+    <div>
+            <!-- 头部 -->
+            <mt-header title="传智34期" ref="header"></mt-header>
+            <router-view class="tmpl" :appRefs="$refs"></router-view>
+
+            <mt-tabbar v-model="selected" ref="footer">
+              <mt-tab-item id="home">
+                <img slot="icon" src="../static/img/index.png">
+                首页
+              </mt-tab-item>
+              <mt-tab-item id="member">
+                <img slot="icon" src="../static/img/vip.png">
+                会员
+              </mt-tab-item>
+              <mt-tab-item id="shopcart">
+                <img slot="icon" src="../static/img/shopcart.png">
+                购物车
+              </mt-tab-item>
+              <mt-tab-item id="search">
+                <img slot="icon" src="../static/img/find.png">
+                查找
+              </mt-tab-item>
+            </mt-tabbar>
+    </div>
+</template>
+<script>
+    export default {
+        data(){
+            return {
+                selected:'',
+            }
+        },
+        watch:{
+            selected(newV){
+                // console.log(newV);
+                //让锚点值改变
+                this.$router.push({
+                    name:newV
+                });
+            }
+        }
+    }
+</script>
+<style scoped>
+   .mint-tabbar{
+    position: fixed;
+    bottom:0;
+   }
+</style>
